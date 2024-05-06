812d6d36adbe57db42539b9febbd954b0b48a271
Julianertle <julian_ertle@web.de>
2024-05-07 00:31:17 +0200

    setup initial code for ultrasonic sensor & heltec

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,30 @@
-#include <Wire.h>
+#include <Arduino.h>
 
-#include "SSD1306Wire.h"
-SSD1306Wire display(0x3C, 4, 15);
-
+#include <Ultrasonic.h>
 
 
-void setup_ssd1306()
-{
-    Serial.begin(921600); // Initialize serial communication at 115200 baud rate
-    Wire.begin(21, 22);                // Initialize the I2C communication with the display
-    display.init();                    // Initialize the display
-    display.setFont(ArialMT_Plain_16); // Set the font for the display
-    display.clear();                   // Clear the display
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.drawString(0, 0, "Hello World");
-    display.display();
+const int TRIG_PIN = 12;    // HC-SR04 Trig pin
+const int ECHO_PIN = 13;    // HC-SR04 Echo pin
+
+Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN);	
+
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
-void setup(){
-  setup_ssd1306();
-};
-void loop(){
-  Serial.println("Test");
-  delay(1000);
-  display.clear();                   // Clear the display
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0, 0, "Hello World");
-  display.display();
-};
+void loop() {
 
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  Serial.print("Sensor: ");
+  Serial.print(ultrasonic.read()); // Prints the distance on the default unit (centimeters)
+  Serial.println("cm");
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+
+
+}
