82881353f1039e5a9a6fcc757af61949fe9f0b03
Julianertle <julian_ertle@web.de>
2024-05-07 16:46:55 +0200

    printed Hello world to the build-in display

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <Ultrasonic.h>
 
+/////// DISPLAY STUFF
+#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
+SSD1306Wire display(0x3c, 4, 15);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
+///////
+
 const int TRIG_PIN = 12;    // HC-SR04 Trig pin
 const int ECHO_PIN = 13;    // HC-SR04 Echo pin
 
@@ -10,13 +16,27 @@ Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN);
 
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(16, OUTPUT);
+  digitalWrite(16, LOW);
+  delay(50);
+  digitalWrite(16, HIGH);
+
+  display.init();
+  display.flipScreenVertically();
+  //display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+  //pinMode(LED_BUILTIN, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);  // Set TRIG_PIN (pin 12) as output
   pinMode(ECHO_PIN, INPUT);   // Set ECHO_PIN (pin 13) as input
   Serial.begin(9600);
 }
 
 void loop() {
+  display.clear();
+  display.drawString(4, 25, "Hello world 1");
+  //display.drawString(128, 54, String(millis()));
+  display.display();
 
   digitalWrite(LED_BUILTIN, HIGH);
 
