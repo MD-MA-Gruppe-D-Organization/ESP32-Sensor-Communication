4121549081898d5c12898cd3fe398a7a92f866bf
Julianertle <julian_ertle@web.de>
2024-06-04 16:26:36 +0200

    added docker compose and mosquitto files

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 /////// DISPLAY STUFF
 #include <Wire.h>               
 #include "SSD1306Wire.h"        
+#include <ESP32Ping.h>
 SSD1306Wire display(0x3c, 4, 15);                           // ADDRESS, SDA, SCL
 ///////
 
@@ -14,50 +15,14 @@ SSD1306Wire display(0x3c, 4, 15);                           // ADDRESS, SDA, SCL
 const char* ssid = "Galaxy S22 661F";                       // Mobile Hotspot Julian
 const char* password = "uolp1538";
 
-const char* mqtt_server = "a367aa99f5bd41b8aaaf16c83e749b9f.s1.eu.hivemq.cloud";
-const int mqtt_port = 8883;  
+//const char* mqtt_server = "192.168.0.37";                
+const char* mqtt_server = "localhost";                
+const int mqtt_port = 1883;  
 const char* topic = "Distance";
-const char* mqtt_username = "jertle";
-const char* mqtt_password = "6EAzFg2EU5&hcSkrmfSw";
+
 
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
-
-// HiveMQ Cloud Let's Encrypt CA certificate
-static const char *root_ca PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
-TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
-cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
-WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
-ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
-MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
-h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
-0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
-A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
-T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
-B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
-B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
-KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
-OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
-jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
-qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
-rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
-HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
-hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
-ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
-3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
-NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
-ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
-TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
-jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
-oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
-4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
-mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
-emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
------END CERTIFICATE-----
-)EOF";
-///////
 
 const int TRIG_PIN = 12;                                    // HC-SR04 Trig pin
 const int ECHO_PIN = 13;                                    // HC-SR04 Echo pin
@@ -155,7 +120,7 @@ void printDistanceToSerialMonitor(int distance){
   Serial.println("cm");
 }
 
-void reconnectCallback(char* topic, byte* payload, unsigned int length) {
+void reconnectCallbackk(char* topic, byte* payload, unsigned int length) {
   // Optional callback function to handle messages received from the broker
   // You can use this to implement logic based on received data
   Serial.print("Received message on topic: ");
@@ -171,7 +136,7 @@ void reconnect() {
     Serial.println("\nAttempting MQTT connection... ");
     String clientId = "ESP32Client-";
     clientId += String(WiFi.macAddress());
-    if (client.connect(clientId.c_str(),mqtt_username,mqtt_password)) {
+    if (client.connect(clientId.c_str())) {
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
@@ -190,9 +155,8 @@ void setup() {
   Serial.begin(9600);
 
   connectToWifi(); 
-  espClient.setCACert(root_ca);
   client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(reconnectCallback);                                  // Optional callback for reconnection logic
+  client.setCallback(reconnectCallbackk);                                  // Optional callback for reconnection logic
   }
 
 void loop() {
@@ -228,4 +192,17 @@ void loop() {
       Serial.println("Data sent: " + String(distance) + "cm");
     }
   } 
+  
+/*   const IPAddress remote_ip(192, 168, 0, 37);        
+
+  Serial.print("Pinging ip ");
+  Serial.println(remote_ip);
+  
+  if(Ping.ping(remote_ip)) {
+    Serial.println("Success!!");
+  } else {
+    Serial.println("Error :(");
+  }
+ */
+
 }
